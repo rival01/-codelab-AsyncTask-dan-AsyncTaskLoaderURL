827eb7cd1fb7347042f27819373964975094dd46
Rival <rivalramlimursat748@gmail.com>
2022-11-16 12:43:48 +0700

    two

--- app/src/main/java/com/val/android/whowroteit/FetchBook.java
+++ app/src/main/java/com/val/android/whowroteit/FetchBook.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.example.android.whowroteit;
+package com.val.android.whowroteit;
 
 import android.os.AsyncTask;
 import android.widget.TextView;
